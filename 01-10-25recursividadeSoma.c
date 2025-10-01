d31b36bfd67f569f3736273656f6fcd37383871d
Acacia Calegari <acacia.ufrj@gmail.com>
2025-10-01 11:34:08 -0300

    Update and rename recursividade01-10-25 to 01-10-25recursividadeSoma.c

--- 01-10-25recursividadeSoma.c
+++ 01-10-25recursividadeSoma.c
@@ -6,14 +6,16 @@
 
 int soma(int n) {
 // Caso base
-if (n == 1) {
-return 1;
-}
+  if (n == 1) {
+  return 1;
+  }
+  
 // Caso recursivo
 return n + soma(n-1);
 }
+
 int main() {
-int resultado = soma(45); // 15
-printf("Soma: %d\n", resultado);
-return 0;
+  int resultado = soma(45); 
+    printf("Soma: %d\n", resultado);
+    return 0;
 }
