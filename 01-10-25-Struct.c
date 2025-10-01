cc35e42d4525503930dd6765ba4a99e481fb261a
Acacia Calegari <acacia.ufrj@gmail.com>
2025-10-01 12:48:11 -0300

    Create 01-10-25-Struct.c

---
+++ 01-10-25-Struct.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// 1. Declaração da struct Ponto
+struct Ponto {
+float x;
+float y;
+};
+
+
+int main() {
+    
+// 2. Declaração de variáveis do tipo struct Ponto
+struct Ponto p1; // variável p1 tipo Ponto
+struct Ponto p2 = {10.0, 20.0}; //Inicializa p2
+
+// 3. Acessando e atribuindo valores aos membros
+p1.x = 5.0;
+p1.y = 8.0;
+
+printf("Ponto p1: (%f, %f)\n", p1.x, p1.y);
+// Saída: (5.0, 8.0)
+printf("Ponto p2: (%f, %f)\n", p2.x, p2.y);
+// Saída: (10.0, 20.0)
+return 0;
+}
