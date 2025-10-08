590119e4a158381c130f8edad975c76f9734e939
Acacia Calegari <acacia.ufrj@gmail.com>
2025-10-08 13:56:39 -0300

    Create 08-10-LoopUsandoWhile.c

---
+++ 08-10-LoopUsandoWhile.c
@@ -0,0 +1,12 @@
+//loop estilo contador d 1 ao 12
+
+#include <stdio.h>
+int main() {
+int numero = 1;
+while (numero <= 12) {
+printf("Número: %d\n", numero);
+numero++; // incrementa
+}
+printf("Loop finalizado!\n");
+return 0;
+}
