9be31489636e8223aa3422980e7e6a2ab5f535d4
Acacia Calegari <acacia.ufrj@gmail.com>
2025-11-04 17:02:50 -0300

    Create 04-11piramide.c

---
+++ 04-11piramide.c
@@ -0,0 +1,15 @@
+//04-11-25
+//parece uma piramide
+//padrao triangular - slide d matrizes e lacos
+
+    int main() {
+    int i, j;
+    
+    for(i = 1; i <= 5; i++) {
+        for(j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    //se qser pd add aki um return 0; 
+}
