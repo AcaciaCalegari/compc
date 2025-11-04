84e7c6659e2583ea75c0c67c01e6dd6120d46959
Acacia Calegari <acacia.ufrj@gmail.com>
2025-11-04 17:14:58 -0300

    Refactor main function to use local variables

--- 04-11piramide.c
+++ 04-11piramide.c
@@ -4,11 +4,9 @@
 
 #include <stdio.h> 
 
-    int main() {
-    int i, j;
-    
-    for(i = 1; i <= 5; i++) {
-        for(j = 1; j <= i; j++) {
+    int main(){
+    for(int i = 1; i <= 5; i++) {
+        for(int j = 1; j <= i; j++) {
             printf("* ");
         }
         printf("\n");
